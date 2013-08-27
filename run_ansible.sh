979c6a84b667348824b9235e047aef8822889689
Mathieu Levesque-Lavallee <mathieu.levesque.lavallee@gmail.com>
2013-08-27 17:00:14 -0400

    Configure Vagrant and Ansible

---
+++ run_ansible.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+WORK_DIR=${1:-"."}
+
+# Install Ansible if it is not installed
+if hash ansible 2>/dev/null; then
+    echo "ansible is already installed"
+else
+    apt-get update
+    apt-get -y install python-software-properties
+    add-apt-repository -y ppa:rquillo/ansible
+    apt-get update
+    apt-get -y install ansible
+fi
+
+# Run Ansible
+ansible-playbook --inventory-file=$WORK_DIR/playbook/"ansible_hosts" $WORK_DIR/playbook/"playbook.yml"
